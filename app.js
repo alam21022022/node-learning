a92052bba746f87597baeabde3980ec1f5bc0004
alam21022022 <sajjad.alam@fissionlabs.com>
2022-08-22 13:07:07 +0530

    Error handling

--- app.js
+++ app.js
@@ -37,6 +37,43 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+// Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+  /*
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+  */
+  // Define Error message
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // Define Error Status Code
+  err.status = 'fail';
+  err.statusCode = 404;
+  /*
+  so that our error handling middleware
+  can then use them in the next step.
+  Q. But now, how do we actually read that In next step?
+  Ans : next middleware. So, when ever we pass anything or arguments In next function,
+        no matter what it is,
+        Express will automatically assume that It gonna be an error.
+        And that applies to every next function in every single middleware anywhere in our application.
+  */
+  next(err);
+});
+
+// Global error handler (that catch part in all routes handler)
+// ye err or 4 argument ko dekh kr he pahchaan jaiga k ye global error handler hai
+// 500 is use for Network error
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 app.listen(8000, () => {
   console.log(`Server is running to the port ${PORT}`);
 });
